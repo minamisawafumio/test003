2085091dfa9a5045773a5b989c4a311e563d73df
南沢郁男 <fmsoft@yahoo.co.jp>
2021-05-14 02:31:38 +0900

    a

--- json/json002/src/main.rs
+++ json/json002/src/main.rs
@@ -1,11 +1,32 @@
-use serde_json::json;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Point {
+    pub topic1: String,
+    pub topic2: String,
+    pub topic3:String,
+    x: i32,
+    y: i32,
+}
 
 fn main() {
-    let json = json!({
-        "foo": "bar",
-        "hoge": 0,
-        "fuga": true,
-        "piyo": [0.1, 0.2, 0.3],
-    });
-    println!("{}", json);
+    let point = Point {
+        topic1: "My first topic1".to_string(),
+        topic2: "My first topic2".to_string(),
+        topic3: "My first topic3".to_string(),
+             x: 11,
+             y: 22
+    };
+
+    // let serialized = serde_json::to_string(&point).unwrap();     //未整形
+    let serialized = serde_json::to_string_pretty(&point).unwrap(); //整形
+    println!("serialized = {}", serialized);
+
+
+    //
+    let sd: Point = serde_json::from_str(&serialized).unwrap();
+    println!("deserialized - {:?}", sd);
+    println!("deserialized x={}", sd.x);
+    println!("deserialized x={}", sd.x);
+    println!("deserialized y={}", sd.y);
 }
--- json/json003/src/main.rs
+++ json/json003/src/main.rs
@@ -2,16 +2,50 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Point {
+    topic1: String,
+    topic2: String,
+    topic3:String,
     x: i32,
     y: i32,
 }
 
 fn main() {
-    let point = Point { x: 11, y: 22 };
+    let point = Point {
+        topic1: "My first topic1".to_string(),
+        topic2: "My first topic2".to_string(),
+        topic3: "My first topic3".to_string(),
+             x: 11,
+             y: 22
+    };
 
-    let serialized = serde_json::to_string(&point).unwrap();
-    println!("serialized = {}", serialized);  // serialized = {"x":1,"y":2}
+    // let serialized = serde_json::to_string(&point).unwrap();     //未整形
+    let serialized = serde_json::to_string_pretty(&point).unwrap(); //整形
+    println!("serialized = {}", serialized);
 
-    let deserialized: Point = serde_json::from_str(&serialized).unwrap();
-    println!("deserialized - {:?}", deserialized);  // deserialized - Point { x: 1, y: 2 }
+    print_json(point);
+
+    print_serialized_json(serialized);
+
+}
+
+//-----------------------------------------------------------------------
+fn print_serialized_json(i_json: String){
+    println!("★　★　★　★　★　★　★　★　★ print_serialized_json");
+    let sd: Point = serde_json::from_str(&i_json).unwrap();
+    println!("deserialized - {:?}", sd);
+    println!("deserialized x={}", sd.x);
+    println!("deserialized x={}", sd.x);
+    println!("deserialized y={}", sd.y);
+    println!("deserialized topic1={}", sd.topic1);
+    println!("deserialized topic2={}", sd.topic2);
+    println!("deserialized topic3={}", sd.topic3);
+}
+
+//-----------------------------------------------------------------------
+fn  print_json(point: Point){
+    println!("★　★　★　★　★　★　★　★　★ print_json");
+    println!("deserialized - {:?}", point);
+    println!("deserialized x={}", point.x);
+    println!("deserialized x={}", point.x);
+    println!("deserialized y={}", point.y);
 }
